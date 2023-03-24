85bf463f4eb85607834fdf84f031df16f31e78eb
projectnum17 <egor.chornyy@gmail.com>
2023-03-24 22:02:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+new Swiper ('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    grabCursor: true,
+    slidesPerView: 3,
+    spaceBetween: 42,
+    autoHeigh: true,
+    slidesPerGroup: 1,
+    centerSlides: true,
+    loop: true,
+});
